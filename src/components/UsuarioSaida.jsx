f9edf9987549319c8c82565e247687b4b1b36ce9
Fernando Lima <fernandoaslima@hotmail.com>
2022-12-20 21:35:36 -0300

    primeiro commit

--- src/components/UsuarioSaida.jsx
+++ src/components/UsuarioSaida.jsx
@@ -2,15 +2,15 @@ import "./style.css"
 
 export function UsuarioSaida(props) {
     return (
-        <div> 
-            
+        <div>
+
             <p>Nome do usuario: {props.estatico}</p>
             <p>O nome de usuário informado acima foi inserido via (props) de forma estática na linha 16, deste projeto, no componentes React chamados "UsuarioEntrada" </p>
 
             <p>Nome do usuario: {props.dinamico}</p>
-            <p>O nome de usuário informado acima foi coletado atravez de caixa de (input) na linha 12 e foi inserido via (props) de forma dinâmica na linha 16, deste projeto, 
+            <p>O nome de usuário informado acima foi coletado atravez de caixa de (input) na linha 12 e foi inserido via (props) de forma dinâmica na linha 16, deste projeto,
                 no componente React do projeto chamados "UsuarioEntrada". O dado coletado é atualizado e inserido em tempo real no componente "UsuarioSaida" e mostrado no campo "Nome de usuário" acima deste paragrafo.</p>
         </div>
 
     )
-    }
+}
